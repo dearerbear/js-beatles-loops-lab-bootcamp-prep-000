c893e0cc6701bb954c1014e184e51ffe5060e215
Dear Bear <dearerbear@gmail.com>
2018-08-09 04:45:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,10 +4,8 @@ var instruments = ["Uke", "Mando", "Sweet Harmonium"]
 
 function theBeatlesPlay(musicians, instruments){
   storage = [];
-  for (let i=0, musicians[i], i++) {
+  for (let i=0; musicians[i]; i++) {
     storage.push(instruments[i])
   }
   console.log(storage)
 }
-
-
